b7084ca1503bdc258ff3f18512f56735bbaec3d1
Roderick <roderickfung@gmail.com>
2016-10-25 05:11:07 -0700

    fixed redirect

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -11,7 +11,8 @@ class ParticipantsController < ApplicationController
       redirect_to 'https://www.eventbrite.com/e/startup-hacks-2017-business-startup-hackathon-for-youth-tickets-27669172239?ref=eweb'
     else
       flash[alert] = @participant.errors.full_messages.to_sentence
-      redirect_to root_path
+      # redirect_to root_path
+      redirect_to 'https://www.eventbrite.com/e/startup-hacks-2017-business-startup-hackathon-for-youth-tickets-27669172239?ref=eweb'
     end
 
   end
