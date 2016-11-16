44037d1a99710b4986def382c50fb3f7466cfa2d
Roderick <roderickfung@gmail.com>
2016-11-15 20:42:46 -0800

    map bug fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require turbolinks
 //= require underscore
 //= require gmaps/google
+//= require turbolinks
 //= require particles.min
 //= require_tree .
