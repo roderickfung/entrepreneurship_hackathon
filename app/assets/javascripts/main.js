1a9750236da31187d2d343cbbe073c763c798bdf
Roderick <roderickfung@gmail.com>
2017-01-17 21:07:33 -0800

    site redesign

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,13 +3,15 @@ $(document).ready(function(){
 
     // nav-transition javascript
 
-    // window.addEventListener('scroll', function(){
-    //   // if (window.scrollY > 15) {
-    //   //   $('.navbar-default').addClass('show');
-    //   // } else {
-    //   //   $('.navbar-default').removeClass('show');
-    //   // }
-    // }, false);
+    window.addEventListener('scroll', function(){
+      if (window.scrollY > 100) {
+        $('.navbar-default').addClass('show');
+        $('#nav-btn').addClass('btn-scroll');
+      } else {
+        $('.navbar-default').removeClass('show');
+        $('#nav-btn').removeClass('btn-scroll');
+      }
+    }, false);
 
   });
 });
